90c2267711a4c703c7f627bfffe8602f1ba9b239
Alex-1R <3125182@smsd.org>
2026-01-12 19:06:54 -0600

    Create Intake.java

---
+++ src/main/java/frc/robot/Subsytems/Intake.java
@@ -0,0 +1,13 @@
+package frc.robot.Subsytems;
+
+public class Intake {
+    public static final class IntakeConstants {
+        public static final double intakeSpeed = 5; // Placeholder 
+        public static final double ejectSpeed = -5; // Placeholder 
+        
+
+        public static final double PROPORTIONAL_GAIN = 0; // Placeholder 
+        public static final double INTEGRAL_GAIN = 0; // Placeholder 
+        public static final double DERIVITIVE
+    }
+}
