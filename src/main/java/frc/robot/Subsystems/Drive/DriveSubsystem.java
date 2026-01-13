3707b58bad9570192184609da52cf158d850e773
Alex-1R <3125182@smsd.org>
2026-01-13 17:49:16 -0600

    Update DriveSubsystem.java

--- src/main/java/frc/robot/Subsystems/Drive/DriveSubsystem.java
+++ src/main/java/frc/robot/Subsystems/Drive/DriveSubsystem.java
@@ -26,7 +26,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.CANIDs.DriveIDs;
+import frc.robot.Constants.CANBus.DriveIDS;
 import frc.robot.Subsystems.Vision.VisionData;
 
 /**
