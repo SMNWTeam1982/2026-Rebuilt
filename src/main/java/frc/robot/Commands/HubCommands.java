af1d06e65073adb0f483e56a4e168ed361276ddd
Zach4346074545723 <3091322@smsd.org>
2026-01-22 15:25:19 -0600

    Update HubCommands.java

--- src/main/java/frc/robot/Commands/HubCommands.java
+++ src/main/java/frc/robot/Commands/HubCommands.java
@@ -3,7 +3,6 @@ package frc.robot.Commands;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.Measured.FieldMeasurements;
 import frc.robot.Constants.Tunables.FieldTunables;
 import frc.robot.Subsystems.Drive.DriveSubsystem;
@@ -23,8 +22,6 @@ public final class HubCommands {
         return drive.orbitPoint(orbitDirection, hubCenter, FieldTunables.HUB_SCORING_DISTANCE);
     }
 
-    public static double calculateFutureDistanceFromHub(Translation2d robotPosition, Translation2d hubPosition, ChassisSpeeds robotVelocity)
-
     public static Translation2d calculateScoringTarget(Translation2d robotPosition, Translation2d hubPosition, ChassisSpeeds robotVelocity){
         double distanceFromHub = robotPosition.getDistance(hubPosition);
 
@@ -34,15 +31,6 @@ public final class HubCommands {
             robotVelocity.vxMetersPerSecond * flightTime,
             robotVelocity.vyMetersPerSecond * flightTime
         );
-
-        //Translation2d futureRobotPosition = robotPosition.plus(robotTravel);
-
-        //double futureDistanceFromHub = futureRobotPosition.getDistance(hubPosition);
-
-        // // if the distance from the hub changes by a negligable amount in the future then the shot confidence is high
-        // if (Math.abs(futureDistanceFromHub - distanceFromHub) <= ShooterTunables.SHOOTING_POSITION_TOLERANCE){
-            
-        // }
 
         // move the hub in the opposite direction of the robots travel to simulate where it going to be when the ball lands
         return hubPosition.minus(robotTravel);
