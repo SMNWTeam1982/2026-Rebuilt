d554c9e017c8dcd92043f9eb944c6e22d580de71
Alex-1R <3125182@smsd.org>
2026-01-24 09:44:09 -0600

    Update Shooter.java

--- src/main/java/frc/robot/Subsystems/Shooter/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter/Shooter.java
@@ -1,44 +1,91 @@
 package frc.robot.Subsystems.Shooter;
 
+import java.util.function.Supplier;
+
 import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
-import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.CANBus.IntakeIDS;
 import frc.robot.Constants.CANBus.ShooterIDs;
+import frc.robot.Constants.Measured.FieldMeasurements;
 
-public class Shooter{
+
+public class Shooter extends SubsystemBase {
     
     public static final class ShooterConstants{
+        // all placeholder values pretty much
         public static final double FIRING_SPEED_ROTATIONS_PER_SECOND = 0; 
-        
         public static final SparkBaseConfig INTAKE_MOTOR_CONFIG = new SparkMaxConfig().smartCurrentLimit(35).idleMode(SparkBaseConfig.IdleMode.kCoast);
+        public static final SparkBaseConfig RIGHT_MOTOR_CONFIG = new SparkMaxConfig().smartCurrentLimit(35).idleMode(SparkBaseConfig.IdleMode.kCoast);
+
+        public static final double SHOOTER_PIDCONST_P = 0.0; 
+        public static final double SHOOTER_PIDCONST_I = 0.0;
+        public static final double SHOOTER_PIDCONST_D = 0.0;
+
+        public static final double SHOOTER_FEEDFORWARDCONST_KS = 0.0;
+        public static final double SHOOTER_FEEDFORWARDCONST_KV = 0.0;      
 
     }
 
-    private final SparkMax leftShooterMotor = new SparkMax(ShooterIDs.LEFT_MOTOR_ID, SparkMax.MotorType.kBrushless); 
-    private final RelativeEncoder leftShooterMotorEncoder = leftShooterMotor.getEncoder();
+    // private final SparkMax leftShooterMotor = new SparkMax(ShooterIDs.LEFT_MOTOR_ID, SparkMax.MotorType.kBrushless); 
+    // private final RelativeEncoder leftShooterMotorEncoder = leftShooterMotor.getEncoder();
 
     private final SparkMax rightShooterMotor = new SparkMax(ShooterIDs.RIGHT_MOTOR_ID, SparkMax.MotorType.kBrushless); 
     private final RelativeEncoder rightShooterMotorEncoder = rightShooterMotor.getEncoder();
 
+    private final PIDController shooterPIDController = new PIDController(ShooterConstants.SHOOTER_PIDCONST_P, ShooterConstants.SHOOTER_PIDCONST_I, ShooterConstants.SHOOTER_PIDCONST_D);
+    private final SimpleMotorFeedforward shooterFeedforward = new SimpleMotorFeedforward(ShooterConstants.SHOOTER_FEEDFORWARDCONST_KS, ShooterConstants.SHOOTER_FEEDFORWARDCONST_KV);
+    private Supplier<Double> getDistanceToHub;
+
+
+    public Shooter(Supplier<Double> getDToHub) {
+        rightShooterMotor.configure(ShooterConstants.RIGHT_MOTOR_CONFIG, 
+        ResetMode.kResetSafeParameters, 
+        PersistMode.kPersistParameters);
+
+        getDistanceToHub = getDToHub;
+        
+        rightShooterMotorEncoder.setPosition(0.0);
+
+        shooterPIDController.setTolerance(.05); 
+        
+    }
+
+    public Command setVelocity(double targetRPM){
+        return runOnce(
+            () -> {
+                double feedforwardVoltage = shooterFeedforward.calculate(targetRPM / 60.0); 
+                double pidOutput = shooterPIDController.calculate((getRightMotorSpeed() / 60.0));
+                double totalOutput = feedforwardVoltage + pidOutput;
+
+                if (totalOutput > 12.0) {
+                    totalOutput = 12.0;
+                } else if (totalOutput < -12.0) {
+                    totalOutput = -12.0;
+                }
+                rightShooterMotor.setVoltage(totalOutput);
+            }
+        );
+    }
+
+    public Command runPID() {
+        return run(() -> {
+            setVelocity(FieldMeasurements.hubDistanceToFlywheelRPM(getDistanceToHub.get()));
+        });
+    }
+
     public double getRightMotorSpeed(){
-        return rightShooterMotor.get
+        // in RPM
+        return rightShooterMotorEncoder.getVelocity();
     }
-
-    public double getLeftMotorSpeed(){
-
-    }
-
 
     
+
 
 }
