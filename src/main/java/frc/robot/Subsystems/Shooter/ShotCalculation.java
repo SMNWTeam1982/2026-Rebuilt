9e0940035ce5fe4ad1a573f9a9e4cb13f2b8c3d4
Zach4346074545723 <3091322@smsd.org>
2026-01-28 11:08:47 -0600

    change to target calculation method
    changed the multi-step target prediction from a recursive function to a loop.

--- src/main/java/frc/robot/Subsystems/Shooter/ShotCalculation.java
+++ src/main/java/frc/robot/Subsystems/Shooter/ShotCalculation.java
@@ -2,51 +2,50 @@ package frc.robot.Subsystems.Shooter;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import frc.robot.Constants.Measured.FieldMeasurements;
 import frc.robot.Constants.Measured.ShooterMeasurements;
+import frc.robot.Constants.Tunables.ShooterTunables;
+import java.util.Arrays;
 
 public final class ShotCalculation {
-    public static Translation2d calculateScoringTarget(
-            Translation2d robotPosition, Translation2d hubPosition, ChassisSpeeds robotVelocity) {
-        double distanceFromHub = robotPosition.getDistance(hubPosition);
+    public static Translation2d getNearestHubPosition(Translation2d robotPosition) {
+        return robotPosition.nearest(
+                Arrays.asList(FieldMeasurements.BLUE_HUB_CENTER, FieldMeasurements.RED_HUB_CENTER));
+    }
 
+    /**
+     * predicts where the hub will be after the robot moves during a shot
+     */
+    public static Translation2d calculateNextHubPosition(
+            Translation2d robotPosition, Translation2d hubPosition, ChassisSpeeds robotVelocity) {
+
+        // use the distance from the hub to calculate the flight time
+        double distanceFromHub = robotPosition.getDistance(hubPosition);
         double flightTime = ShooterMeasurements.hubDistanceToFlightTime(distanceFromHub);
 
+        // calculate how far the robot moves during that time
         Translation2d robotTravel = new Translation2d(
                 robotVelocity.vxMetersPerSecond * flightTime, robotVelocity.vyMetersPerSecond * flightTime);
 
-        // move the hub in the opposite direction of the robots travel to simulate where it going to be when the ball
-        // lands
+        // move the hub in the opposite direction of the robot travel to simulate were it will be after the flight time
         return hubPosition.minus(robotTravel);
     }
 
-    public static Translation2d calculateScoringTargetMultiStep(
-            Translation2d robotPosition, Translation2d hubPosition, ChassisSpeeds robotVelocity, int steps) {
+    /**
+     * since the flight time changes when the hub position changes, we recalculate the values several times to converge
+     * on an ideal target
+     */
+    public static Translation2d calculateHubPositionMultiStep(
+            Translation2d robotPosition, ChassisSpeeds robotVelocity, int steps) {
 
-        // the first position is the actual position of the hub
-        if (steps == 0) {
-            return hubPosition;
+        // the first position is the actual hub position
+        Translation2d hubPosition = getNearestHubPosition(robotPosition);
+
+        for (int stepsDone = 0; stepsDone < steps; stepsDone++) {
+            // calculate the next hub position based on the previous hub position
+            hubPosition = calculateNextHubPosition(robotPosition, hubPosition, robotVelocity);
         }
 
-        // get the previous position
-        Translation2d previousHubPosition =
-                calculateScoringTargetMultiStep(robotPosition, hubPosition, robotVelocity, steps - 1);
-
-        // use the previous position to calculate the new flight time
-        double distanceFromHub = robotPosition.getDistance(previousHubPosition);
-        double flightTime = ShooterMeasurements.hubDistanceToFlightTime(distanceFromHub);
-
-        // calculate new robot travel
-        Translation2d robotTravel = new Translation2d(
-                robotVelocity.vxMetersPerSecond * flightTime, robotVelocity.vyMetersPerSecond * flightTime);
-
-        // return the new hub position
-        return hubPosition.minus(robotTravel);
-    }
-
-    public static double calculateRPMMultiStep(
-            Translation2d robotPosition, Translation2d hubPosition, ChassisSpeeds robotVelocity, int steps) {
-        Translation2d futureHubTarget =
-                calculateScoringTargetMultiStep(robotPosition, hubPosition, robotVelocity, steps);
-        return ShooterMeasurements.hubDistanceToFlywheelRPM(robotPosition.getDistance(futureHubTarget));
+        return hubPosition;
     }
 }
