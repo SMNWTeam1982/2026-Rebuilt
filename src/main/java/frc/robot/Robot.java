13b331ac3f5161bfd6b8269d0ce5e0ff8933c3d2
ZZZTNAHSI <166968547+ZZZTNAHSI@users.noreply.github.com>
2026-01-29 00:48:36 +0000

    Automated code formatting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,13 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import org.littletonrobotics.junction.LogFileUtil;
-import org.littletonrobotics.junction.LoggedRobot;
-import org.littletonrobotics.junction.Logger;
-import org.littletonrobotics.junction.networktables.NT4Publisher;
-import org.littletonrobotics.junction.wpilog.WPILOGReader;
-import org.littletonrobotics.junction.wpilog.WPILOGWriter;
-import edu.wpi.first.wpilibj.Filesystem;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
