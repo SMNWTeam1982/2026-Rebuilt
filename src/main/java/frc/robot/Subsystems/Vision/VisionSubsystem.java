7dc979a039d2780afd47dc535d6dd8f6887d38e0
Alex-1R <3125182@smsd.org>
2026-01-29 10:40:38 -0600

    Update VisionSubsystem.java

--- src/main/java/frc/robot/Subsystems/Vision/VisionSubsystem.java
+++ src/main/java/frc/robot/Subsystems/Vision/VisionSubsystem.java
@@ -2,15 +2,7 @@ package frc.robot.Subsystems.Vision;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
