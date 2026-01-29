5e5b289ada6aa194fcafe4433053ed3b05175714
Alex-1R <181907310+Alex-1R@users.noreply.github.com>
2026-01-29 01:56:33 +0000

    Automated code formatting

--- src/main/java/frc/robot/Subsystems/Vision/VisionSubsystem.java
+++ src/main/java/frc/robot/Subsystems/Vision/VisionSubsystem.java
@@ -2,27 +2,17 @@ package frc.robot.Subsystems.Vision;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import frc.robot.Constants.CANBus.VisionConstants;
 import java.util.Optional;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import frc.robot.Constants.CANBus.VisionConstants;
 
 /** Optional Calling, if the target is not found, this subsystem will not be called */
-
 public class VisionSubsystem extends SubsystemBase {
 
     private final PhotonCamera instanceCamera;
@@ -33,7 +23,7 @@ public class VisionSubsystem extends SubsystemBase {
 
     private Optional<VisionData> lastVisionResult;
 
-    public VisionSubsystem(Transform3d cameraRelativeToRobot, String cameraName) { 
+    public VisionSubsystem(Transform3d cameraRelativeToRobot, String cameraName) {
         photonPoseEstimator = new PhotonPoseEstimator(
                 AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField),
                 PoseStrategy.LOWEST_AMBIGUITY,
@@ -44,13 +34,10 @@ public class VisionSubsystem extends SubsystemBase {
         setDefaultCommand(pollVisionData());
     }
 
-
-    public Command pollVisionData(){ // Factory
-        return run(
-            () -> {
-                lastVisionResult = getVisionResult();
-            }
-        );
+    public Command pollVisionData() { // Factory
+        return run(() -> {
+            lastVisionResult = getVisionResult();
+        });
     }
 
     public Optional<VisionData> getLastVisionResult() {
@@ -66,7 +53,7 @@ public class VisionSubsystem extends SubsystemBase {
 
         for (var result : instanceCamera.getAllUnreadResults()) {
             lastEstimatedPose = photonPoseEstimator.update(result);
-        } 
+        }
         // if getAllUnreadResults() is empty then lastEstimatedPose will be Optional.empty()
         // also accounts for results that have data but are surrounded by results without data
 
@@ -83,6 +70,4 @@ public class VisionSubsystem extends SubsystemBase {
                 // in their example code
                 ));
     }
-
-    
 }
