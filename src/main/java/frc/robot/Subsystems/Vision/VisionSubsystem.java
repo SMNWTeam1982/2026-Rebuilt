6c5eac9b8a9b0015511b1e7e5727b2806286e1db
Alex-1R <3125182@smsd.org>
2026-02-02 17:56:52 -0600

    "Bare Bones" vision data created

--- src/main/java/frc/robot/Subsystems/Vision/VisionSubsystem.java
+++ src/main/java/frc/robot/Subsystems/Vision/VisionSubsystem.java
@@ -2,27 +2,19 @@ package frc.robot.Subsystems.Vision;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import java.util.List;
 import java.util.Optional;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import org.photonvision.targeting.PhotonTrackedTarget;
+import frc.robot.Constants.Tunables.VisionTunables;
 
 /** To do:
  * Bare bones vision control ( no checks, no ambiguity)
  * pull request after
  * Then start working on ambiguity if necessary/if have time
- * 
- * 
  */
 
 /** Optional Calling, if the target is not found, this subsystem will not be called */
@@ -33,17 +25,12 @@ public class VisionSubsystem extends SubsystemBase {
     private final String cameraName;
     private Optional<VisionData> lastVisionResult;
 
-    private Matrix<N3, N1> PHOTON_CAM_VISION_TRUST = VecBuilder.fill(0.5, 0.5, 1);   
-    private Matrix<N3, N1> SINGLE_TAG_STANDARD_DEVIATION = VecBuilder.fill(4, 4, 8);
-    private Matrix<N3, N1> MULTPLE_TAG_STANDARD_DEVIATION = VecBuilder.fill(0.5, 0.5, 1);
-
     public VisionSubsystem(Transform3d cameraRelativeToRobot, String cameraName) {
         photonPoseEstimator = new PhotonPoseEstimator(
             AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField), 
             cameraRelativeToRobot);
         instanceCamera = new PhotonCamera(cameraName);
         this.cameraName = cameraName;
-
         setDefaultCommand(pollVisionData());
     }
 
@@ -65,12 +52,11 @@ public class VisionSubsystem extends SubsystemBase {
         Optional<EstimatedRobotPose> lastEstimatedPose = Optional.empty();
 
         for (var result : instanceCamera.getAllUnreadResults()) {
+            // Estimates the average position of the targets based on the targets last position
             lastEstimatedPose = photonPoseEstimator.estimateAverageBestTargetsPose(result);
         }
-
         // if getAllUnreadResults() is empty then lastEstimatedPose will be Optional.empty()
         // also accounts for results that have data but are surrounded by results without data
-
         if (lastEstimatedPose.isEmpty()) {
             return Optional.empty();
         }
@@ -80,58 +66,8 @@ public class VisionSubsystem extends SubsystemBase {
         return Optional.of(new VisionData(
                 estimatedPose.estimatedPose.toPose2d(),
                 estimatedPose.timestampSeconds,
-                PHOTON_CAM_VISION_TRUST // we should calculate this the same way photonVision does
+                VisionTunables.PHOTON_CAM_VISION_TRUST // we should calculate this the same way photonVision does
                 // in their example code
                 ));
     }
-
-    /** Calculates new standard deviations based on the number of tags, estimation strategy, and distance from tags
-     * @param visionEstimation estimated position from this is used to guess standard deviations
-     * @param targets Includes every target in the camera frame
-     */
-    public void updateEstimationStdDevs(
-            Optional<EstimatedRobotPose> visionEstimation, List<PhotonTrackedTarget> targets) {
-        if (visionEstimation.isEmpty()) { // No position estimated, default to the standard deviation value
-            PHOTON_CAM_VISION_TRUST = SINGLE_TAG_STANDARD_DEVIATION;
-        } else { // Position estimated
-            var estimateStandardDeviation = SINGLE_TAG_STANDARD_DEVIATION;
-            int numTags = 0;
-            double averageDistance = 0;
-
-            // find number of tags found, get estimated distance metric
-            for (var tag : targets) {
-                /** Finds the tag position by searching through every tag on the field, gets the position of the tag
-                 * found based on its id.
-                 */
-                var tagPosition = photonPoseEstimator.getFieldTags().getTagPose(tag.getFiducialId());
-                if (tagPosition.isEmpty()) continue; // if no tags are found, nothing happens
-                numTags++;
-                averageDistance += tagPosition
-                        .get()
-                        .toPose2d()
-                        .getTranslation()
-                        .getDistance(visionEstimation
-                                .get()
-                                .estimatedPose
-                                .toPose2d()
-                                .getTranslation()); // edit the estimatedPose
-            }
-            if (visionEstimation.isEmpty()) { // No position estimated, default to the standard deviation value
-                PHOTON_CAM_VISION_TRUST = SINGLE_TAG_STANDARD_DEVIATION;
-            } else { // Position estimated
-                averageDistance /= numTags;
-                if (numTags > 1) {
-                    estimateStandardDeviation = MULTPLE_TAG_STANDARD_DEVIATION;
-                }
-                if (numTags == 1 && averageDistance > 4) {
-                    estimateStandardDeviation = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
-                } else {
-                    estimateStandardDeviation =
-                            estimateStandardDeviation.times(1 + (averageDistance * averageDistance / 30));
-                    PHOTON_CAM_VISION_TRUST = estimateStandardDeviation;
-                }
-            }
-        }
-    }
-
 }
