47b67128f9ea8c584e81a7c1e0ae2ee5b8695449
Zach4346074545723 <3091322@smsd.org>
2026-02-04 18:22:33 -0600

    update vendor dep

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 85;
-  public static final String GIT_SHA = "078fd80351d6dbd9bcfa6662150f7afa099b63ba";
-  public static final String GIT_DATE = "2026-01-31 13:12:45 CST";
-  public static final String GIT_BRANCH = "Advantage-Kit";
-  public static final String BUILD_DATE = "2026-02-02 17:41:09 CST";
-  public static final long BUILD_UNIX_TIME = 1770075669708L;
+  public static final int GIT_REVISION = 149;
+  public static final String GIT_SHA = "6903ad53a9d4f1031743c09ce294d8549fc14617";
+  public static final String GIT_DATE = "2026-02-04 18:16:08 CST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-02-04 18:21:39 CST";
+  public static final long BUILD_UNIX_TIME = 1770250899634L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
