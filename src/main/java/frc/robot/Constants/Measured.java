3c64e0dca1b26eaf6cd346f2aacf11aacadafa92
Cougar Robotics <3127118@smsd.org>
2026-02-07 13:32:26 -0600

    Updated Translation module constants

--- src/main/java/frc/robot/Constants/Measured.java
+++ src/main/java/frc/robot/Constants/Measured.java
@@ -19,10 +19,11 @@ import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Measured {
     public static final class DriveBaseMeasurements {
-        public static final Translation2d FRONT_LEFT_TRANSLATION = new Translation2d(0.2635, 0.2635);
-        public static final Translation2d FRONT_RIGHT_TRANSLATION = new Translation2d(0.2635, -0.2635);
-        public static final Translation2d BACK_LEFT_TRANSLATION = new Translation2d(-0.2635, 0.2635);
-        public static final Translation2d BACK_RIGHT_TRANSLATION = new Translation2d(-0.2635, -0.2635);
+        //0.271653
+        public static final Translation2d FRONT_LEFT_TRANSLATION = new Translation2d(0.271653, 0.271653);
+        public static final Translation2d FRONT_RIGHT_TRANSLATION = new Translation2d(0.271653, -0.271653);
+        public static final Translation2d BACK_LEFT_TRANSLATION = new Translation2d(-0.271653, 0.271653);
+        public static final Translation2d BACK_RIGHT_TRANSLATION = new Translation2d(-0.271653, -0.271653);
 
         /** the fastest speed the robot can go, derived from testing */
         public static final double PHYSICAL_MAX_SPEED = 3.8;
