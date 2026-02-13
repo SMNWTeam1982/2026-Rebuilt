bd84a1d758b0043498c0d4c051be01c925335dcd
Zach4346074545723 <181798494+Zach4346074545723@users.noreply.github.com>
2026-02-13 18:50:02 +0000

    Automated code formatting

--- src/main/java/frc/robot/Subsystems/Drive/DriveSubsystem.java
+++ src/main/java/frc/robot/Subsystems/Drive/DriveSubsystem.java
@@ -4,7 +4,6 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import com.pathplanner.lib.util.PathPlannerLogging;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
