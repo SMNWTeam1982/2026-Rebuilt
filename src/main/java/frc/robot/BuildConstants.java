a520147221f7da5f8e5acb1f90bfddbcb0c6aeb3
CougarRobotics1982 <cougarrobotics1982@gmail.com>
2026-03-28 14:08:38 -0500

    Update at 'Sat Mar 28 14:08:37 CDT 2026'

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 366;
-  public static final String GIT_SHA = "18171e5ad08cce57688faeae3d5453e24576dc8b";
-  public static final String GIT_DATE = "2026-03-28 12:04:32 CDT";
+  public static final int GIT_REVISION = 367;
+  public static final String GIT_SHA = "7454b5181d994be924a9cd283ad2c51166a8e43c";
+  public static final String GIT_DATE = "2026-03-28 13:17:32 CDT";
   public static final String GIT_BRANCH = "event_st-louis";
-  public static final String BUILD_DATE = "2026-03-28 12:04:33 CDT";
-  public static final long BUILD_UNIX_TIME = 1774717473875L;
+  public static final String BUILD_DATE = "2026-03-28 13:17:33 CDT";
+  public static final long BUILD_UNIX_TIME = 1774721853685L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/Constants/Tunables.java
+++ src/main/java/frc/robot/Constants/Tunables.java
@@ -34,7 +34,7 @@ public class Tunables {
          * configurable based on driver preference and game need
          * <p> this will be capped by the artificial max speed, so it can be set to any value & used to tune sensitivity
          * <p> meters/sec */
-        public static final double DRIVE_SPEED = 3.0; // 1 m/s
+        public static final double DRIVE_SPEED = 2.5; // 1 m/s
         /** this can end up being capped by the artificial max speed, but the cap depends on robot size & module positions
          * <p> radians/sec
          */
@@ -68,7 +68,7 @@ public class Tunables {
 
         /** config for the drive motor on the module */
         public static final SparkBaseConfig DRIVE_MOTOR_CONFIG =
-                new SparkMaxConfig().smartCurrentLimit(40).inverted(true).idleMode(SparkBaseConfig.IdleMode.kBrake);
+                new SparkMaxConfig().smartCurrentLimit(35).inverted(true).idleMode(SparkBaseConfig.IdleMode.kBrake);
 
         /** config for the turn motor on the module */
         public static final SparkBaseConfig TURN_MOTOR_CONFIG =
