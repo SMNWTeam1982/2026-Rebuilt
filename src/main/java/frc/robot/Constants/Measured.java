f19f83144addd3c2781dbc4821efca0d2bd680f8
ArnoldoAlvbh <181798382+ArnoldoAlvbh@users.noreply.github.com>
2026-04-01 22:57:44 +0000

    Automated code formatting

--- src/main/java/frc/robot/Constants/Measured.java
+++ src/main/java/frc/robot/Constants/Measured.java
@@ -105,7 +105,6 @@ public class Measured {
     }
 
     public static final class ShooterMeasurements {
-       
 
         public static final double MAX_SCHOOL_FLYWHEEL_RPM =
                 4750; // the balls will hit the cieling in the cafeteria above this speed
