f09d524d573be76f1569a10284aec97ed96af9cd
ArnoldoAlvbh <3127118@smsd.org>
2026-04-07 16:42:33 -0500

    Auto
    Can someone who has Path planner installed check if my auto looks good?

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 371;
-  public static final String GIT_SHA = "ea19dac709e8b1183c43ee849af7365728531abb";
-  public static final String GIT_DATE = "2026-03-28 15:42:29 CDT";
-  public static final String GIT_BRANCH = "event_st-louis";
-  public static final String BUILD_DATE = "2026-03-28 15:42:31 CDT";
-  public static final long BUILD_UNIX_TIME = 1774730551006L;
+  public static final int GIT_REVISION = 351;
+  public static final String GIT_SHA = "81e955bf6b97b9220e695c3b4276249366b9e16b";
+  public static final String GIT_DATE = "2026-04-04 13:49:25 CDT";
+  public static final String GIT_BRANCH = "defense-mode";
+  public static final String BUILD_DATE = "2026-04-04 13:59:37 CDT";
+  public static final long BUILD_UNIX_TIME = 1775329177409L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
