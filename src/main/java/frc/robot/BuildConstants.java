d274f85bdd12645ae034a8abef70e8c87e7bf23a
CH <me@pascalrr.gay>
2026-04-09 17:06:02 -0500

    Update at 'Thu Apr 09 17:06:02 CDT 2026'

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 392;
-  public static final String GIT_SHA = "a34d91bf28d5158e71ded1f9a84e923daa4f63bd";
-  public static final String GIT_DATE = "2026-04-09 17:05:32 CDT";
+  public static final int GIT_REVISION = 393;
+  public static final String GIT_SHA = "2cb4b890c5d945642f8cd060a781dd5ab1c17549";
+  public static final String GIT_DATE = "2026-04-09 17:05:46 CDT";
   public static final String GIT_BRANCH = "event_GKC";
-  public static final String BUILD_DATE = "2026-04-09 17:05:32 CDT";
-  public static final long BUILD_UNIX_TIME = 1775772332957L;
+  public static final String BUILD_DATE = "2026-04-09 17:05:47 CDT";
+  public static final long BUILD_UNIX_TIME = 1775772347077L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
