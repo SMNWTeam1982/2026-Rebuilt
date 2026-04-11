645ee4ba4cf2b3a3127e4d68b10320ab9fe0d88f
CougarRobotics1982 <cougarrobotics1982@gmail.com>
2026-04-11 08:34:54 -0500

    Update at 'Sat Apr 11 08:34:54 CDT 2026'

--- src/main/java/frc/robot/Constants/Tunables.java
+++ src/main/java/frc/robot/Constants/Tunables.java
@@ -166,15 +166,15 @@ public class Tunables {
         /** the maximuma mount of time that the intake will run the pivot motor during a retract attempt */
         public static final Time RETRACT_ATTEMPT_TIME = Seconds.of(1.5);
 
-        public static final double PIVOT_OUTPUT_RATE_LIMIT = 8.0;
+        public static final double PIVOT_OUTPUT_RATE_LIMIT = 4.0;
 
         public static final double PIVOT_MOVE_IN_SPEED = 0.8;
-        public static final double PIVOT_MOVE_OUT_SPEED = -0.8;
+        public static final double PIVOT_MOVE_OUT_SPEED = -0.4;
 
         // percent that the intake will be set at when intaking
-        public static final double INTAKE_SPEED = 0.8;
+        public static final double INTAKE_SPEED = -0.8;
 
-        public static final double INTAKE_REVERSE_SPEED = -0.8;
+        public static final double INTAKE_REVERSE_SPEED = 0.8;
 
         /** if the motor is drawing more than this and is moving slow then the motor is considered jammed */
         public static final double INTAKE_MOTOR_JAM_CURRENT_THRESHHOLD = 15.0;
